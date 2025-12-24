5f230f719d0542f79f05c435ef9acfbef010a608
melisapastin <melisa.pastin@student.upt.ro>
2025-12-24 21:56:57 +0200

    login -> main

--- app/src/main/java/com/example/munchly/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/munchly/ui/screens/ProfileScreen.kt
@@ -1,1 +1,32 @@
-qqq
+package com.example.munchly.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun ProfileScreen(
+    username: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = username,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF8B4513)
+        )
+    }
+}
